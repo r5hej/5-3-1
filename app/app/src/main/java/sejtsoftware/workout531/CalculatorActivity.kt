8580c6ea8954ad1c808ac857a6c4e2769605b340
Marius Lauge Nørgaard <mariuslauge@gmail.com>
2018-03-08 11:20:31 +0100

    app initial commit

---
+++ app/app/src/main/java/sejtsoftware/workout531/CalculatorActivity.kt
@@ -0,0 +1,12 @@
+package sejtsoftware.workout531
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CalculatorActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
