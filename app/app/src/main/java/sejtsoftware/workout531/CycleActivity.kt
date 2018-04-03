f4e7fe828306d1c8856044c45b153445cd1c59ad
r5hej <charliebyrdam@gmail.com>
2018-04-03 14:13:25 +0200

    Added a fragment, not working currently

--- app/app/src/main/java/sejtsoftware/workout531/CycleActivity.kt
+++ app/app/src/main/java/sejtsoftware/workout531/CycleActivity.kt
@@ -1,5 +1,7 @@
 package sejtsoftware.workout531
 
+import android.app.Fragment
+import android.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -8,5 +10,7 @@ class CycleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cycle)
+
+
     }
 }
--- app/app/src/main/java/sejtsoftware/workout531/database_helper/Database.java
+++ app/app/src/main/java/sejtsoftware/workout531/database_helper/Database.java
@@ -4,6 +4,6 @@ package sejtsoftware.workout531.database_helper;
 import android.arch.persistence.room.RoomDatabase;
 
 @android.arch.persistence.room.Database(entities = {CycleEntity.class}, version = 1)
-public abstract class Database extends RoomDatabase{
+public abstract class Database extends RoomDatabase {
     public abstract CycleDao cycleDao();
 }
