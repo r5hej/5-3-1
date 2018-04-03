38c24ce948520745b9c77215beab29e61dc7ff70
Marius Lauge Nørgaard <mariuslauge@gmail.com>
2018-04-03 13:52:06 +0200

    maybe now?

---
+++ app/app/src/main/java/sejtsoftware/workout531/database_helper/Excercise.java
@@ -0,0 +1,28 @@
+package sejtsoftware.workout531.database_helper;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class Excercise {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name="name")
+    private String name;
+
+    @ColumnInfo(name="cycle")
+    private int cycle;
+
+    // Save the calculated 1RM: max(weight*reps*.0333 + weight)
+    // Also save the weight and reps used to calculate this 1RM, for user display.
+    @ColumnInfo(name="oneRm")
+    private int oneRepMax;
+
+    @ColumnInfo(name="oneRmReps")
+    private int oneRmReps;
+
+    @ColumnInfo(name="oneRmWeight")
+    private int oneRmWeight;
+}
