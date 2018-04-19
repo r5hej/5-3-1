b854228700d4dc70a781e6105ae301596b325a51
Marius Lauge Nørgaard <mariuslauge@gmail.com>
2018-04-19 09:04:52 +0200

    Small morning commit before coffee

--- app/app/src/main/java/sejtsoftware/workout531/helpers/database/AppRoomDatabase.java
+++ app/app/src/main/java/sejtsoftware/workout531/helpers/database/AppRoomDatabase.java
@@ -3,7 +3,7 @@ package sejtsoftware.workout531.helpers.database;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(entities = {Cycle.class, ActiveRMValues.class}, version = 1)
+@Database(entities = {Cycle.class, ActiveRMValues.class}, version = 1, exportSchema = false)
 public abstract class AppRoomDatabase extends RoomDatabase {
     public abstract CycleDao cycleDao();
 
